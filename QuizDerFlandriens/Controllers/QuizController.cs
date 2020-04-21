b4a056449f85af900bee3de3e509953bad149769
RobbeDeClercq <43599650+RobbeDeClercq@users.noreply.github.com>
2020-04-21 16:15:40 +0200

    Play Quiz Flow finished + controles on quiz

--- QuizDerFlandriens/Controllers/QuizController.cs
+++ QuizDerFlandriens/Controllers/QuizController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using QuizDerFlandriens.Models;
 using QuizDerFlandriens.Models.Repositories;
@@ -12,10 +14,12 @@ namespace QuizDerFlandriens.Controllers
     public class QuizController : Controller
     {
         private IQuizRepo quizRepo;
+        private UserManager<Person> userManager;
 
-        public QuizController(IQuizRepo quizRepo)
+        public QuizController(IQuizRepo quizRepo, UserManager<Person> userManager)
         {
             this.quizRepo = quizRepo;
+            this.userManager = userManager;
         }
 
         public async Task<IActionResult> Quizzes()
@@ -33,9 +37,9 @@ namespace QuizDerFlandriens.Controllers
             }
             return View(quizzes);
         }
-        public async Task<IActionResult> PlayQuiz(Guid id)
+        public IActionResult PlayQuiz(Guid id)
         {
-            IEnumerable<Question> questions = await quizRepo.GetAllQuestionsByQuizId(id);
+            //IEnumerable<Question> questions = await quizRepo.GetAllQuestionsByQuizId(id);
             return RedirectToAction(nameof(ShowQuestion), new { quizId = id, currentQuestion = 0, score = 0 });
         }
 
@@ -43,17 +47,19 @@ namespace QuizDerFlandriens.Controllers
         {
             IEnumerable<Question> questions = await quizRepo.GetAllQuestionsByQuizId(quizId);
             List<Question> questionsList = questions.ToList();
+            float progressPercentage = (float.Parse(currentQuestion.ToString())/float.Parse(questionsList.Count().ToString()))*100;
+            ViewData["ProgressPercentage"] = progressPercentage;
             ViewData["PotentialScore"] = currentQuestion;
             ViewData["Score"] = score;
             if(currentQuestion == questionsList.Count())
             {
-                return RedirectToAction(nameof(EndQuiz));
+                return RedirectToAction(nameof(EndQuiz), new { score = score, maxScore = currentQuestion, quizId = quizId });
             }
             Question question = await quizRepo.GetQuestionForIdAsync(questionsList[currentQuestion].Id);
             return View(question);
         }
 
-        public async Task<IActionResult> CheckAnswer(bool isCorrect, Guid quizId, int currentQuestion, int score)
+        public IActionResult CheckAnswer(bool isCorrect, Guid quizId, int currentQuestion, int score)
         {
             if (isCorrect)
             {
@@ -63,9 +69,32 @@ namespace QuizDerFlandriens.Controllers
             return RedirectToAction(nameof(ShowQuestion), new { quizId = quizId, currentQuestion = currentQuestion, score = score });
         }
 
-        private async Task<IActionResult> EndQuiz()
+        public async Task<IActionResult> EndQuiz(int score, int maxScore, Guid QuizId)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            Quiz quiz = await quizRepo.GetQuizForIdAsync(QuizId);
+            Result result = new Result()
+            {
+                Id = Guid.NewGuid(),
+                Score = score,
+                QuizId = QuizId,
+                Quiz = quiz
+            };
+            string personId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            Person person = await userManager.FindByIdAsync(personId);
+            result.Person = person;
+            ViewData["MaxScore"] = maxScore;
+            ViewData["PersonName"] = person.Name;
+            ViewData["QuizName"] = quiz.Subject;
+            var created = await quizRepo.AddResult(result);
+            if (created == null)
+            {
+                throw new Exception("Invalid Entry");
+            }
+            return View(result);
         }
 
 
